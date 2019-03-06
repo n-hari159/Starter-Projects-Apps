47438bb15a17e3f72be0d99a70ae2cea43f2dc6d
n-hari159 <n.hari159@gmail.com>
2019-03-06 23:34:35 +0530

    Initial Commit

---
+++ Business_Model_Canvas/BMC2.js
@@ -0,0 +1,47 @@
+var currentTab=0;
+showTab(currentTab);
+
+function showTab(n){
+    var x=document.getElementsByClassName("inner");
+    x[n].style.display="block";
+    
+    if(n==0){
+        document.getElementById("prevBtn").style.display = "none";
+    } else {
+        document.getElementById("prevBtn").style.display = "inline";
+    }
+    if (n == (x.length - 1)) {
+        document.getElementById("nextBtn").innerHTML = "Submit";
+    } else {
+        document.getElementById("nextBtn").innerHTML = "Next";
+    }
+}
+
+function nextPrev(n){
+    var x = document.getElementsByClassName("inner");
+    if (n == 1 && !validateForm()){
+        alert("Kindly fill the required fields, Darling !!")
+        return false;
+    }
+    x[currentTab].style.display="none";
+    currentTab = currentTab + n;
+    if (currentTab >= x.length) {
+        alert("Thanks");
+        document.getElementById("wizard").submit();
+        return false;
+    } 
+    showTab(currentTab);
+}
+
+function validateForm() {
+    var x, y, i, valid = true;
+    x = document.getElementsByClassName("inner");
+    y = x[currentTab].getElementsByTagName("input");
+    for (i = 0; i < y.length; i++) {
+      if (y[i].value == "") {
+        y[i].className += " invalid";
+        valid = false;
+      }
+    }
+    return valid;
+  }
